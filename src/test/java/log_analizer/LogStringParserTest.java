e39044cf2e54a82b77f1518a038e570ace299215
valyalshikovd <770vda@mail.ru>
2024-10-29 00:43:53 +0300

    test: update test

--- src/test/java/log_analizer/LogStringParserTest.java
+++ src/test/java/log_analizer/LogStringParserTest.java
@@ -2,6 +2,7 @@ package log_analizer;
 
 import backend.academy.log_analizer.LogString;
 import backend.academy.log_analizer.LogStringParserImpl;
+import backend.academy.log_analizer.juice.ObjectFabric;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +13,7 @@ public class LogStringParserTest {
 
         String log = "80.91.33.133 - - [17/May/2015:08:05:50 +0000] \"GET /downloads/product_1 HTTP/1.1\" 304 0 \"-\" \"Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.22)\"";
 
-        LogString logString = new LogStringParserImpl().parseLogString(log);
+        LogString logString = ObjectFabric.getObject(LogStringParserImpl.class).parseLogString(log);
 
         assertEquals(log, logString.toString());
 
