3f86c1f190ef59a717924d4a2d9e51fb173fb063
valyalshikovd <770vda@mail.ru>
2024-10-28 17:15:36 +0300

    add: LogStringParser

---
+++ src/main/java/backend/academy/log_analizer/LogStringParser.java
@@ -0,0 +1,6 @@
+package backend.academy.log_analizer;
+
+public interface LogStringParser {
+
+    LogString parseLogString(String logString);
+}
---
+++ src/main/java/backend/academy/log_analizer/LogStringParserImpl.java
@@ -0,0 +1,13 @@
+package backend.academy.log_analizer;
+
+public class LogStringParserImpl implements LogStringParser {
+
+    @Override
+    public LogString parseLogString(String logString) {
+        try {
+
+        } catch (Exception e) {
+
+        };
+    }
+}
