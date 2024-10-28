1c4aca590722e4ee59347ece8380dadca91cd969
valyalshikovd <770vda@mail.ru>
2024-10-29 00:37:03 +0300

    add: add ObjectFabric & GuiceModule

---
+++ src/main/java/backend/academy/log_analizer/juice/GuiceModule.java
@@ -0,0 +1,18 @@
+package backend.academy.log_analizer.juice;
+
+import backend.academy.log_analizer.LogStringParser;
+import backend.academy.log_analizer.LogStringParserImpl;
+import backend.academy.log_analizer.ZoneDateParserImpl;
+import backend.academy.log_analizer.ZoneDateTimeParser;
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+
+
+public class GuiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(LogStringParser.class).to(LogStringParserImpl.class).in(Scopes.SINGLETON);
+        bind(ZoneDateTimeParser.class).to(ZoneDateParserImpl.class).in(Scopes.SINGLETON);
+    }
+}
