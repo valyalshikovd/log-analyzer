1550f9571db537959311f83c7653deb0d4475be7
valyalshikovd <770vda@mail.ru>
2024-10-29 00:22:18 +0300

    fix & refactor: fix toString()

--- src/main/java/backend/academy/log_analizer/LogString.java
+++ src/main/java/backend/academy/log_analizer/LogString.java
@@ -1,6 +1,8 @@
 package backend.academy.log_analizer;
 
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,27 +28,31 @@ public class LogString {
     private String httpUserAgent;
 
     @Override public String toString() {
-        return  remoteAddr +
-            " " + remoteHost +
-            " " + httpXForwardedFor +
-            " " + timeLocal +
-            " " + request +
-            " " + status +
-            " " + bodyBytesSent +
-            " " + httpReferer +
-            " " + httpUserAgent;
+        return  remoteAddr
+            + " " + remoteHost
+            + " " + httpXForwardedFor
+            + " [" + timeLocal.format(DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH))
+            + "] " + request
+            + " " + status
+            + " " + bodyBytesSent
+            + " " + httpReferer
+            + " " + httpUserAgent;
     }
 
     @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LogString logString = (LogString) o;
-        return Objects.equals(remoteAddr, logString.remoteAddr) && Objects.equals(remoteHost, logString.remoteHost) &&
-            Objects.equals(httpXForwardedFor, logString.httpXForwardedFor) &&
-            Objects.equals(timeLocal, logString.timeLocal) && Objects.equals(request, logString.request) &&
-            Objects.equals(status, logString.status) && Objects.equals(bodyBytesSent, logString.bodyBytesSent) &&
-            Objects.equals(httpReferer, logString.httpReferer) &&
-            Objects.equals(httpUserAgent, logString.httpUserAgent);
+        return Objects.equals(remoteAddr, logString.remoteAddr) && Objects.equals(remoteHost, logString.remoteHost)
+            && Objects.equals(httpXForwardedFor, logString.httpXForwardedFor)
+            && Objects.equals(timeLocal, logString.timeLocal) && Objects.equals(request, logString.request)
+            && Objects.equals(status, logString.status) && Objects.equals(bodyBytesSent, logString.bodyBytesSent)
+            && Objects.equals(httpReferer, logString.httpReferer)
+            && Objects.equals(httpUserAgent, logString.httpUserAgent);
     }
 
     @Override
