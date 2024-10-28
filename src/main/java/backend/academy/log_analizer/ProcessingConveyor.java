fecdd84bba402be41a837f67dd49a9048f5475cd
valyalshikovd <770vda@mail.ru>
2024-10-29 01:06:42 +0300

    add: ProcessingConveyor

---
+++ src/main/java/backend/academy/log_analizer/ProcessingConveyor.java
@@ -0,0 +1,32 @@
+package backend.academy.log_analizer;
+
+import jakarta.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ProcessingConveyor {
+
+
+    private final LogStringParser logStringParser;
+
+    @Inject
+    public ProcessingConveyor(LogStringParser logStringParser) {
+        this.logStringParser = logStringParser;
+    }
+
+    public void process(String path_string) {
+
+        Path path = Paths.get(path_string);
+
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.map(logStringParser::parseLogString).forEach(System.out::println);
+        } catch (IOException e) {
+            return;
+        }
+
+    }
+
+}
