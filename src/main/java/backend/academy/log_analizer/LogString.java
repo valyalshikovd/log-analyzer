bb8cde2428fd3dc49bfa37857339a3d5aa6302dc
valyalshikovd <770vda@mail.ru>
2024-10-28 18:24:16 +0300

    add: add toString(), equals(), hashcode()

--- src/main/java/backend/academy/log_analizer/LogString.java
+++ src/main/java/backend/academy/log_analizer/LogString.java
@@ -1,6 +1,7 @@
 package backend.academy.log_analizer;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,4 +24,34 @@ public class LogString {
     private Integer bodyBytesSent;
     private String httpReferer = DEFAULT_VALUE;
     private String httpUserAgent;
+
+    @Override public String toString() {
+        return  remoteAddr +
+            " " + remoteHost +
+            " " + httpXForwardedFor +
+            " " + timeLocal +
+            " " + request +
+            " " + status +
+            " " + bodyBytesSent +
+            " " + httpReferer +
+            " " + httpUserAgent;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LogString logString = (LogString) o;
+        return Objects.equals(remoteAddr, logString.remoteAddr) && Objects.equals(remoteHost, logString.remoteHost) &&
+            Objects.equals(httpXForwardedFor, logString.httpXForwardedFor) &&
+            Objects.equals(timeLocal, logString.timeLocal) && Objects.equals(request, logString.request) &&
+            Objects.equals(status, logString.status) && Objects.equals(bodyBytesSent, logString.bodyBytesSent) &&
+            Objects.equals(httpReferer, logString.httpReferer) &&
+            Objects.equals(httpUserAgent, logString.httpUserAgent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(remoteAddr, remoteHost, httpXForwardedFor, timeLocal, request, status, bodyBytesSent,
+            httpReferer, httpUserAgent);
+    }
 }
