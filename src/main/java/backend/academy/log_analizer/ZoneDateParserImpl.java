13ae67548d1d346d8e2a69f6100b4a95f543192b
valyalshikovd <770vda@mail.ru>
2024-10-29 11:19:56 +0300

    add: add tests ZoneDateParser

--- src/main/java/backend/academy/log_analizer/ZoneDateParserImpl.java
+++ src/main/java/backend/academy/log_analizer/ZoneDateParserImpl.java
@@ -8,6 +8,9 @@ public class ZoneDateParserImpl implements ZoneDateTimeParser {
 
     private final static String PATTERN = "dd/MMM/yyyy:HH:mm:ss Z";
 
+    /**
+     * Строка в параметр должна быть в таком виде, с квадратными скобками [17/May/2015:08:05:25 +0000]
+     */
     @Override
     public ZonedDateTime zonedDateTimeParse(String logString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN, Locale.ENGLISH);
---
+++ src/test/java/log_analizer/ZoneDateParserTest.java
@@ -0,0 +1,35 @@
+package log_analizer;
+
+import backend.academy.log_analizer.ZoneDateParserImpl;
+import backend.academy.log_analizer.juice.ObjectFabric;
+import org.instancio.internal.generator.time.ZoneIdGenerator;
+import org.junit.jupiter.api.Test;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ZoneDateParserTest {
+
+    @Test
+    public void zoneDateParseTest(){
+        ZoneDateParserImpl zoneDateParser = ObjectFabric.getObject(ZoneDateParserImpl.class);
+        ZonedDateTime zonedDateTime = zoneDateParser.zonedDateTimeParse("[17/May/2015:08:05:25 +0000]");
+        ZonedDateTime expected = ZonedDateTime.of(2015,5,17,
+            8, 5,25, 0, ZoneId.of("+0000"));
+        assertEquals(expected, zonedDateTime);
+    }
+
+    @Test
+    public void zoneDateParseInvalidData1Test(){
+        ZoneDateParserImpl zoneDateParser = ObjectFabric.getObject(ZoneDateParserImpl.class);
+        assertThrows(DateTimeParseException.class, () -> zoneDateParser.zonedDateTimeParse("17/May/2015:08:05:25 +0000"));
+    }
+
+    @Test
+    public void zoneDateParseInvalidData2Test(){
+        ZoneDateParserImpl zoneDateParser = ObjectFabric.getObject(ZoneDateParserImpl.class);
+        assertThrows(DateTimeParseException.class, () -> zoneDateParser.zonedDateTimeParse("[17/May/2015:08:05:25]"));
+    }
+}
