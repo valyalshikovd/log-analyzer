29877f4bf9c0c7b8fd47f2c15d0d347e88522086
valyalshikovd <770vda@mail.ru>
2024-10-30 19:19:37 +0300

    fix: fix percentile calculating

--- src/main/java/backend/academy/log_analizer/ProcessingConveyor.java
+++ src/main/java/backend/academy/log_analizer/ProcessingConveyor.java
@@ -1,8 +1,18 @@
 package backend.academy.log_analizer;
 
+import backend.academy.log_analizer.statisticCollector.Renderer;
+import backend.academy.log_analizer.statisticCollector.RendererImpl;
 import backend.academy.log_analizer.statisticCollector.StatisticCollectorComposer;
 import backend.academy.log_analizer.statisticCollector.collector.AverageResponseSizeCollector;
 import backend.academy.log_analizer.statisticCollector.collector.CountCollector;
+import backend.academy.log_analizer.statisticCollector.collector.FrequentResourcesCollector;
+import backend.academy.log_analizer.statisticCollector.collector.FrequentStatusCollector;
+import backend.academy.log_analizer.statisticCollector.collector.PercentileCollector;
+import backend.academy.log_analizer.statisticCollector.rendereSegment.AverageResponseSizeRenderer;
+import backend.academy.log_analizer.statisticCollector.rendereSegment.CountRendererSegment;
+import backend.academy.log_analizer.statisticCollector.rendereSegment.FrequentResourcesRendererSegment;
+import backend.academy.log_analizer.statisticCollector.rendereSegment.FrequentStatusRendererSegment;
+import backend.academy.log_analizer.statisticCollector.rendereSegment.PercentileRenderer;
 import jakarta.inject.Inject;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -33,13 +43,39 @@ public class ProcessingConveyor {
         );
 
         StatisticCollectorComposer collector = new StatisticCollectorComposer();
-
         collector.addCollector(
-            new CountCollector("Количество запросов: ")
+            new CountCollector("Количество запросов")
         );
-
         collector.addCollector(
             new AverageResponseSizeCollector("Средний размер ответа")
+        );
+        collector.addCollector(
+            new PercentileCollector("Перцентиль")
+        );
+        collector.addCollector(
+            new FrequentResourcesCollector("Наиболее запрашиваемые ресурсы", 5)
+        );
+        collector.addCollector(
+            new FrequentStatusCollector("Наиболее частые статусы ответов", 5)
+        );
+
+        Renderer renderer = new RendererImpl();
+        renderer.addRenderSegment(
+            new CountRendererSegment("Количество запросов")
+        );
+        renderer.addRenderSegment(
+            new AverageResponseSizeRenderer("Средний размер ответа")
+        );
+        renderer.addRenderSegment(
+            new FrequentResourcesRendererSegment("Наиболее запрашиваемые ресурсы")
+        );
+
+        renderer.addRenderSegment(
+            new FrequentStatusRendererSegment("Наиболее частые статусы ответов")
+        );
+
+        renderer.addRenderSegment(
+            new PercentileRenderer("Перцентиль")
         );
 
         try (Stream<String> lines = Files.lines(path)) {
@@ -47,11 +83,11 @@ public class ProcessingConveyor {
                 .map(logStringParser::parseLogString)
                 .filter(chain::checkFilters)
                 .forEach(collector::collect);
-
-         //   System.out.println(collector.getStatistics());
         } catch (IOException e) {
             return;
         }
+
+        System.out.println(renderer.render(collector.getStatistics()));
     }
 
 
