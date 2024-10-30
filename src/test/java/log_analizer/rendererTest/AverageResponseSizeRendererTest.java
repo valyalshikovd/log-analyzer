fa7dca4160c10d3bec551212516c46c56fc9a5f4
valyalshikovd <770vda@mail.ru>
2024-10-30 19:47:14 +0300

    fix: fix AverageResponseSizeRenderer

--- src/test/java/log_analizer/rendererTest/AverageResponseSizeRendererTest.java
+++ src/test/java/log_analizer/rendererTest/AverageResponseSizeRendererTest.java
@@ -1,10 +1,16 @@
 package log_analizer.rendererTest;
 
+import backend.academy.log_analizer.statisticCollector.rendereSegment.AverageResponseSizeRenderer;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AverageResponseSizeRendererTest {
 
     @Test
     void render() {
+
+        AverageResponseSizeRenderer r = new AverageResponseSizeRenderer("s");
+        assertEquals("| Средний размер ответа |254.54b |", r.render("254.54"));
+
     }
 }
