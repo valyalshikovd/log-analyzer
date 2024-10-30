3ec1332b3501f389ccf82f51142a743eb7fed644
valyalshikovd <770vda@mail.ru>
2024-10-30 15:30:56 +0300

    add: FrequentResourcesCollector

---
+++ src/main/java/backend/academy/log_analizer/statisticCollector/collector/FrequentResourcesCollector.java
@@ -0,0 +1,54 @@
+package backend.academy.log_analizer.statisticCollector.collector;
+
+import backend.academy.log_analizer.LogString;
+import backend.academy.log_analizer.statisticCollector.StatisticCollector;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class FrequentResourcesCollector implements StatisticCollector {
+
+    private final HashMap<String, Integer> frequentResources = new HashMap<>();
+
+    private final String id;
+    private final int limit;
+
+    public FrequentResourcesCollector(String id, int limit) {
+        this.id = id;
+        this.limit = limit;
+    }
+
+    @Override
+    public void collectStatistics(LogString logString) {
+
+        if (!frequentResources.containsKey(logString.request()))
+        {
+            frequentResources.put(logString.request(), 1);
+            return;
+        }
+
+        frequentResources.put(logString.request(), frequentResources.get(logString.request()) + 1);
+    }
+
+    @Override
+    public String getStatistics() {
+
+        List<String> sortedKeys = frequentResources.entrySet().stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .map(Map.Entry::getKey)
+            .limit(limit)
+            .toList();
+
+        StringBuilder statistics = new StringBuilder();
+        for (String key : sortedKeys) {
+            statistics.append(key).append(": ").append(frequentResources.get(key)).append("\n");
+        }
+        return statistics.toString();
+    }
+
+    @Override public String toString() {
+        return id;
+    }
+}
