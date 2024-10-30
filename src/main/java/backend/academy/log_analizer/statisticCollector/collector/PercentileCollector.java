80b0f3db9344698fc33abc851d418e5a0819c78c
valyalshikovd <770vda@mail.ru>
2024-10-30 18:56:30 +0300

    add: add PercentileRenderer

---
+++ src/main/java/backend/academy/log_analizer/statisticCollector/collector/PercentileCollector.java
@@ -0,0 +1,35 @@
+package backend.academy.log_analizer.statisticCollector.collector;
+
+import backend.academy.log_analizer.LogString;
+import backend.academy.log_analizer.statisticCollector.StatisticCollector;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PercentileCollector implements StatisticCollector {
+    private int count = 0;
+    private final String id;
+
+    /**
+     * хранить все ответы не эффективно
+     */
+    private List<Integer> list = new ArrayList<>();
+
+    public PercentileCollector(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public void collectStatistics(LogString logString) {
+        count += 1;
+        list.add(logString.status());
+    }
+
+    @Override
+    public String getStatistics() {
+        return list.get((int) Math.round((count + 1) * 0.95)) + "";
+    }
+
+    @Override public String toString() {
+        return id;
+    }
+}
